66e1e154af15f16d0732d4c7c8ddcf38353a60fb
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-11-25 14:21:03 -0300

    feat: server

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const cloudinary = require('cloudinary').v2;
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-const connectDB = require('./config/db');
-
-//Routes
-const authRoutes = require('./routes/auth.routes');
-const userRoutes = require('./routes/user.routes');
-
 dotenv.config();
-
+const connectDB = require('./config/db');
 connectDB();
+
+const authRoutes = require('./routes/auth.routes');
+const categoryRoutes = require('./routes/category.routes');
+const productRoutes = require('./routes/product.routes');
+const imageRoutes = require('./routes/image.routes');
+const cartRoutes = require('./routes/cart.routes');
+const userRoutes = require('./routes/user.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -19,20 +23,37 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
+// Middleware
 const app = express();
-
 app.use(express.json({ limit: '50mb' }));
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+  origin: `${process.env.BASE_URL}`,
+  credentials: true
+}));
+app.use(cookieParser());
 
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/user', userRoutes);
+// Routes
+app.use('/auth', authRoutes);
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
+app.use('/images', imageRoutes);
+app.use('/cart', cartRoutes);
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
 
+app.get('/', (req, res) => {
+  res.json({
+    author: "Rodolfo Wolff",
+    message: "Olá, bem vindo ao server vo di pizza!",
+  });
+});
+
+// Server listenning
 const PORT = process.env.PORT || 5000;
-
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
